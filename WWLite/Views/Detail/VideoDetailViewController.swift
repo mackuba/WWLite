3d4794067e0219859f19a81a52f6dc72381a4118
Kuba Suder <jakub.suder@gmail.com>
2020-08-29 18:14:40 +0200

    displaying video file in the player

--- WWLite/Views/Detail/VideoDetailViewController.swift
+++ WWLite/Views/Detail/VideoDetailViewController.swift
@@ -6,12 +6,14 @@
 //  Copyright © 2020 Kuba Suder. All rights reserved.
 //
 
+import AVKit
 import Cocoa
 
 class VideoDetailViewController: NSViewController {
 
     @IBOutlet weak var titleLabel: NSTextField!
     @IBOutlet weak var descriptionLabel: NSTextField!
+    @IBOutlet weak var playerView: AVPlayerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +25,13 @@ class VideoDetailViewController: NSViewController {
         titleLabel.stringValue = video.title
         descriptionLabel.stringValue = video.description
 
+        if video.isDownloaded, let downloadPath = video.downloadPath {
+            playerView.alphaValue = 1.0
+            playerView.player = AVPlayer(url: downloadPath)
+        } else {
+            playerView.alphaValue = 0.5
+            playerView.player = nil
+        }
         self.view.isHidden = false
     }
 }

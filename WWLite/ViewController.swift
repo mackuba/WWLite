54e452af5b60fc3a5f82b140d10f74170d8e765e
Kuba Suder <jakub.suder@gmail.com>
2020-08-29 15:11:01 +0200

    created new Mac app project

---
+++ WWLite/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WWLite
+//
+//  Created by Kuba Suder on 29.08.2020.
+//  Copyright © 2020 Kuba Suder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

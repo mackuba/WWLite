0d234bd51a96f6024fc7b3916e53950171ae143e
Kuba Suder <jakub.suder@gmail.com>
2020-08-30 21:24:15 +0200

    very simple filtering

--- WWLite/Views/Sidebar/VideoList.swift
+++ WWLite/Views/Sidebar/VideoList.swift
@@ -11,36 +11,63 @@ import SwiftUI
 struct VideoList: View {
     @ObservedObject var dataStore: DataStore
     @Binding var selectedId: String?
+    @State var searchQuery: String = ""
+
+    func matchesQuery(_ video: SessionVideo) -> Bool {
+        if searchQuery.isEmpty {
+            return true
+        } else {
+            let words = searchQuery.lowercased().components(separatedBy: .whitespaces)
+            let title = video.title.lowercased()
+            return words.allSatisfy { title.contains($0) }
+        }
+    }
 
     var body: some View {
-        List(selection: $selectedId) {
-            ForEach(dataStore.videos) { video in
-                VStack {
-                    HStack {
-                        VStack(alignment: .leading, spacing: 3) {
-                            Text(video.title)
-                            Text(video.id).font(.caption)
-                        }
+        VStack(spacing: 0) {
+            TextField("Search", text: $searchQuery)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(5)
 
-                        Spacer()
-
-                        if video.isDownloaded {
-                            Text("⬇")
-                        } else if video.isDownloading {
-                            Text("…")
-                        }
+            List(selection: $selectedId) {
+                ForEach(dataStore.videos) { video in
+                    if self.matchesQuery(video) {
+                        VideoRow(video: video)
+                            .tag(video.id)
+                            .contextMenu(ContextMenu() {
+                                Button("Download") {
+                                    self.dataStore.downloadVideo(id: video.id)
+                                }
+                                .disabled(video.isDownloaded)
+                            })
                     }
-
-                    Divider()
                 }
-                .tag(video.id)
-                .contextMenu(ContextMenu() {
-                    Button("Download") {
-                        self.dataStore.downloadVideo(id: video.id)
-                    }
-                    .disabled(video.isDownloaded)
-                })
             }
+        }
+    }
+}
+
+struct VideoRow: View {
+    var video: SessionVideo
+
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading, spacing: 3) {
+                    Text(video.title)
+                    Text(video.id).font(.caption)
+                }
+
+                Spacer()
+
+                if video.isDownloaded {
+                    Text("⬇")
+                } else if video.isDownloading {
+                    Text("…")
+                }
+            }
+
+            Divider()
         }
     }
 }
